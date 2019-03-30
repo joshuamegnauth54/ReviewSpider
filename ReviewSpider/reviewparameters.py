a5d0282d160c26d3650e6b2c05b537e079ea362d
joshuamegnauth54 <joshua.megnauth54@gmail.com>
2019-03-30 03:31:09 -0400

    Mostly completed reviewparameters. Started actual spider.

--- ReviewSpider/reviewparameters.py
+++ ReviewSpider/reviewparameters.py
@@ -3,28 +3,61 @@
 from dataclasses import dataclass, field
 
 
-@dataclass(frozen = True)
+@dataclass(unsafe_hash=True)
 class ReviewParameters:
-    game: str = field(default=None, metadata={"description":
-          "Title of the game"})
+    __slots__ = ("title", "subtitle", "releasedate", "genres", "sitename",
+                 "url", "score")
 
-    subtitle: str = field(default=None, metadata={"description":
-          "Subtitle of the game if applicable; None otherwise"})
-
-    releasedate: str = field(default=None, metadata={"description":
-         "Release date"})
-
-    genres: set[str] = field(default=None, metadata={"description":
-          "Genres as a set"})
-
-    url: str = field(default=None, metadata={"description":
-         "Review site name"})
-
-    score: float = field(default=None, metadata={"description":
-         "Review score of the game out of 100"})
+    title: str
+    subtitle: str
+    releasedate: str
+    genres: str
+    sitename: str
+    url: str
+    score: float
 
     # Overridden functions
 
     def __str__(self):
         return "Game: {0} Site: {1} Score: {2}".format(self.game,
                       self.url, self.score)
+
+# =============================================================================
+# The equality operator only needs to ensure that the title, release date,
+# site, and review score are equal. We don't need to check the genre as
+# the objects are equal if it's the same score from the same site for
+# the same game.
+# =============================================================================
+
+    def __eq__(self, other):
+        return (self.title == other.title
+            and self.releasedate == other.releasedate
+            and self.sitename == other.sitename
+            and self.score == other.score)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+# =============================================================================
+# Alphabetical order.
+#
+# If the titles are equal then the subtitles are the tiebreakers.
+# We could also add the strings and perform the comparison. This way
+# seems neater.
+# =============================================================================
+
+    def __lt__(self, other):
+        if (self.title == other.title):
+            return self.subtitle < other.subtitle
+        return self.title < other.subtitle
+
+    def __gt__(self, other):
+        return other < self
+
+    def __le__(self, other):
+        if (self.title == other.title):
+            return self.subtitle <= other.subtitle
+        return self.title <= other.subtitle
+
+    def __ge__(self, other):
+        return other <= self
--- ReviewSpider/reviewspider.py
+++ ReviewSpider/reviewspider.py
@@ -11,7 +11,18 @@ class ReviewSpider(scrappy.Spider):
         super().__init__(args, kwargs)
 
         self.name = instancename
-        self.parameters
+        self.parameters = self._checkparameters(parameters)
+        urlsplit = str.split(self.parameters.url, "/")
+
+        if (urlsplit[0] == "http"):
+            allowed_domains = urlsplit[1]
+        else:
+            allowed_domains = urlsplit[0]
+
 
     def _checkparameters(parameters):
+        return parameters #Will implement later if I figure it out
+
+    def dispatch(self, response):
+        pass
 
